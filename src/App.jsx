eebdb51019fae614f75c55d56922c75efbe05777
yuanhong <yuanhong@sina.com>
2019-06-14 18:21:45 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+// import { Button } from 'antd'// import Button from 'antd/lib/button'; 
+import './App'
+import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom'
+import Login from './pages/login/login';
+import Admin from './pages/admin/admin';
+// import 'antd/dist/antd.css';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/login" component={Login}></Route>
+                    <Route path="/admin" component={Admin}></Route>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
+
+}
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './login.less'
+import logo from './images/logo.png'
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+export default class Login extends React.Component {
+
+    render() {
+
+        return (
+            <div className='login'>
+                <header className='header-login'>
+                    <img src={logo} alt="logo" />
+                    <h1>React项目: 后台管理系统</h1>
+                </header>
+                <section className='section-login'>
+                    <h2>用户登陆</h2>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="用户名"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                type="password"
+                                placeholder="密码"
+                            />
+                        </Form.Item>
+                        <Form.Item>            
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                                    登 录
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </section>
+            </div>
+        )
+    }
+}
