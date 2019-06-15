9307b849288a0e42b5b738b8fb7c2bfb76d43e05
yuanhong <yuanhong@sina.com>
2019-06-15 12:54:53 +0800

    sss

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
 import './login.less'
 import logo from './images/logo.png'
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
-export default class Login extends React.Component {
-
+import { Form, Icon, Input, Button } from 'antd';
+const Item = Form.Item
+class Login extends React.Component {
+    handleSubmit = (event) => {
+        // alert('嘤嘤嘤')
+        event.preventDefault()
+        const { form } = this.props
+        form.validateFields((err, values) => {
+            if (!err) {
+                //读取输入的数据
+                const values = this.props.form.getFieldsValue()
+                console.log('发送登录的Ajax请求', values)
+            } else {
+                //表单验证错误
+            }
+        })
+       //清空用户输入
+        // this.props.form.resetFields()
+    }
+    validatorPwd = (rule, value, callback) => {
+        value = value.trim()
+        if (value ==='') { 
+            callback('密码必须输入')
+        } else if (value.length < 6 || value.length > 12) {
+            callback('密码长度必须是6-12位')
+        } else if(!/^[a-zA-Z0-9_]+$/){
+            callback('密码必须是英文，数字，下划线组成')
+        } else {
+            callback()//通过验证
+        }
+    }
+        //收集数据
+        // const username = this.props.form.getFieldValue('username')
+        // const password = this.props.form.getFieldValue('password')
+        // const values = this.props.form.getFieldsValue()
+        // console.log(username, password, values)
+        // this.props.form.resetFields()
+    
     render() {
-
+        const { getFieldDecorator } = this.props.form
         return (
             <div className='login'>
                 <header className='header-login'>
@@ -15,22 +50,38 @@ export default class Login extends React.Component {
                 <section className='section-login'>
                     <h2>用户登陆</h2>
                     <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Item>
+                            {
+                                getFieldDecorator('username', {
+                                    initialValue: '',
+                                    rules: [{ whitespace:true, required: true, message: '必须输入用户名' },
+                                            { min: 5, message: '用户名最小为5个字符' },
+                                            { max: 12, message: '用户名最大为12个字符' },
+                                            {pattern: /^[a-zA-Z0-9_]+$/, message: '必须是英文，数字，下划线组成'}
+                                    ],
+
+                                })(<Input
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    placeholder="用户名"
+                                />)
+                            }
+                        </Item>
                         <Form.Item>
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="用户名"
-                            />
+                            {
+                                getFieldDecorator('password',{
+                                    initialValue: '',
+                                    rules: [{validator: this.validatorPwd}],
+                                })(<Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    placeholder="密码"
+                                />)
+                            }
+
                         </Form.Item>
                         <Form.Item>
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="密码"
-                            />
-                        </Form.Item>
-                        <Form.Item>            
                             <Button type="primary" htmlType="submit" className="login-form-button">
-                                                    登 录
+                                登 录
                             </Button>
                         </Form.Item>
                     </Form>
@@ -39,3 +90,5 @@ export default class Login extends React.Component {
         )
     }
 }
+const Wrapform = Form.create()(Login)
+export default Wrapform
